9d9be6cead0286674445b211fbab86e4c2772eca
Keandrrre <112655348+Keandrrre@users.noreply.github.com>
2022-10-31 12:43:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-contacts-basic-gui-start
+ Start Code for My Contacts Basic GUI Assignment
---
+++ main.js
@@ -0,0 +1,47 @@
+// My Contacts Basic
+
+// HTML Elements
+let goBtnEl = document.getElementById('go-btn');
+let menuEl = document.getElementById('menu');
+let outputEl = document.getElementById('output');
+
+// Go Btn - Menu Listener
+goBtnEl.addEventListener('click', goBtnHandler);
+
+function goBtnHandler() {
+  // Get Menu Selection
+  let selection = menuEl.value;
+
+  if (selection === 'display-all') {
+    displayContacts();
+  } else if (selection === 'add') {
+    addContact();
+  } else if (selection === 'remove') {
+    removeContact();
+  } else if (selection === 'display-name') {
+    displayByName();
+  } else if (selection === 'display-country') {
+    displayByCountry();
+  }
+}
+
+// MENU FUNCTIONS
+function displayContacts() {
+  console.log('Display Contacts');
+}
+
+function addContact() {
+  console.log('Add Contact');
+}
+
+function removeContact() {
+  console.log('Remove Contact');
+}
+
+function displayByName() {
+  console.log('Display by Name');
+}
+
+function displayByCountry() {
+  console.log('Display by Country');
+}
